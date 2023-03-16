b926ac53afeb4bb853bcf46e8e7fe46f4f697565
nada <nadaait171@gmail.com>
2023-03-16 13:27:12 -0700

    printf

---
+++ 0x00-hello_world/printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void)
+{
+	/** this is a c program
+	 * shows a message and return 0
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
