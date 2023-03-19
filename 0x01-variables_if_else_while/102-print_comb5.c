b4c863edd82568dc3a66b011710982d7682e2ae1
nada <nadaait171@gmail.com>
2023-03-19 12:39:09 -0700

    nn

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,18 +11,19 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0 ; i < 100 ; i++)
+	for (i = 0 ; i < 99 ; i++)
 	{
 		for (j = 0 ; j < 100 ; j++)
 		{
 			putchar(i + '0');
-			putchar(' ');
 			putchar(j + '0');
-			if (i + j != 198)
+			if (i != 98 && j != 99)
 			{
 				putchar(',');
 				putchar(' ');
 			}
+			else
+				putchar(' ');
 		}
 	}
 	putchar('\n');
