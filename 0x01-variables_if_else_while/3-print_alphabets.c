8f23b91a77ec34d96a7d11085626919f78018af8
nada <nadaait171@gmail.com>
2023-03-19 09:31:26 -0700

    s

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+        int n;
+
+        for (n = 'a'; n <= 'z'; n++)
+                putchar(n);
+	for (n = 'A'; n <= 'Z'; n++)
+                putchar(n);
+        putchar('\n');
+}
