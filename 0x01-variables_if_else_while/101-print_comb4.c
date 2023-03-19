f898423d36b6f8f85523152c8e65c72ddaa50861
nada <nadaait171@gmail.com>
2023-03-19 11:17:19 -0700

    nn

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - comb
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = i + 1 ; j < 10 ; j++)
+		{
+			for (k = j + 1 ; k < 10 ; k++)
+			{
+				if (i != j && i != k && j != k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					if (i + j + k != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
