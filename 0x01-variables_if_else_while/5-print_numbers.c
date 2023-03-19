9e4e9b78b5606ab5a64083eae9800b4a899dc5c4
nada <nadaait171@gmail.com>
2023-03-19 09:51:36 -0700

    num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
