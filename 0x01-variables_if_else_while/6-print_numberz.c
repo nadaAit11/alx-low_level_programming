7ac12f6d7fb8fd84281126476232af2958ce5f22
nada <nadaait171@gmail.com>
2023-03-19 10:02:24 -0700

    nmz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - numberz
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		putchar((char)i);
+
+	putchar('\n');
+	return (0);
+}
