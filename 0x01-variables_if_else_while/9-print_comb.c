533fcb472dd771fd7f4829693e3887c82014ff51
nada <nadaait171@gmail.com>
2023-03-19 10:25:41 -0700

    space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,10 @@ int main(void)
 	for (i = 0 ; i < 10 ; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
 		if (i == 9)
 			putchar('$');
 		else
-			putchar(', ');
+			putchar(','+' ');
 		putchar('\n');
 	return (0);
 }
