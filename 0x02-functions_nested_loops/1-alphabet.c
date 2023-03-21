7610db52835bbf24f7c641ad9d1c27b8d8b163d0
nada <nadaait171@gmail.com>
2023-03-21 12:11:31 -0700

    nij

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
 #include "_putchar.c"
+
 /**
  * print_alphabet - prints alphabet
  *
  *Return: void
  */
+
 void print_alphabet(void)
 {
 	char i;
 
-	for (i = 'a' ; i < 'z' ; i++)
+	for (i = 'a' ; i <= 'z' ; i++)
 		_putchar(i);
 
 	_putchar('\n');
