bb05daf102a8083b0ebe32079764563d6dbd5695
nada <nadaait171@gmail.com>
2023-03-21 15:56:16 -0700

    h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ *Return:void
+ */
+
+void jack_bauer(void)
+{
+	int s = 0;
+	int m = 0;
+	int h = 0;
+	
+	while (h < 24)
+	{
+	if (s == 9)
+	{
+		m++;
+		s=0;
+		if (m == 59)
+		{
+			m = 0;
+			h++;
+		}
+	}
+	s++;
+	if (h < 10)
+		_putchar('0');
+	_putchar(h + '0');
+	_putchar(':');
+	_putchar(m + '0');
+	_putchar(s + '0');
+
+	}
+}
