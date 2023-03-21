4a853e4287fa0540badbdf1654a0fced34243d14
nada <nadaait171@gmail.com>
2023-03-21 15:22:19 -0700

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print_last_digit
+ *
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int m = n % 10;
+	return (m);
+}
