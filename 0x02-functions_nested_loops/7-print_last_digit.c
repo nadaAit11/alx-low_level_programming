e0527da20add5145ae0979422dfcf8664777a4e1
nada <nadaait171@gmail.com>
2023-03-21 15:38:57 -0700

    nh

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,20 +2,21 @@
 
 /**
  * print_last_digit - print_last_digit
- *
+ *@n : the value
  * Return: the value of the last digit
  */
 
 int print_last_digit(int n)
 {
+	int m;
+
 	if (n < 0)
 		n = -n;
 
-	int m = n % 10;
+	m = n % 10;
 
-	if ( m < 0)
+	if (m < 0)
 		m = -m;
-	
 	_putchar(m + '0');
 	return (m);
 }
