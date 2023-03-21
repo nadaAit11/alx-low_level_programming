0ec031ffea92ac5e84e7392c154468e9a96574f1
nada <nadaait171@gmail.com>
2023-03-21 12:37:52 -0700

    h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - alphabet x 10
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char i,j;
+
+	for (j = 0 ; j < 10 ; j++)
+		for (i = 'a' ; i <= 'z' ; i++)
+			_putchar(i);
+
+	_putchar('\n');
+}
