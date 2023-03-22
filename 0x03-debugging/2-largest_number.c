0c78e09f4cc20068559506e124abf3f1ef18338a
nada <nadaait171@gmail.com>
2023-03-22 08:51:50 -0700

    ng

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,9 +14,12 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 		largest = a;
-	else if (b > a && b > c)
+	else if (a > b && c > a)
+		largest = c;
+	else if (b > c)
 		largest = b;
 	else
 		largest = c;
+
 	return (largest);
 }
