c0b0a3f6a7fb0b2a3d047e4e150835743d708127
nada <nadaait171@gmail.com>
2023-03-23 08:24:01 -0700

    no

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9 without using standard functions
- *
+ * print_numbers - prints 0-9
  *
  * Return: void
  */
 
 void print_numbers(void)
 {
-	int c = 48;
+	int i;
 
-	while (c < 58)
+	for (i = 0 ; i <= 9 ; i++)
 	{
-		_putchar(c);
-		c++;
+	       _putchar(i + '0');
 	}
-	_putchar('\n');
+	
+	_putchar('\n');	
 }
