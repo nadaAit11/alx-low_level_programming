c1bd3b6a0264a5dc30bbd5f9f6868fbc28649bb5
nada <nadaait171@gmail.com>
2023-03-23 08:44:39 -0700

    n

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,15 +2,15 @@
 
 /**
  * print_line - print a straight line
- *
+ * @n : number of characters
  * Return:void
  */
 
 void print_line(int n)
 {
-	if ( n > 0)
+	if (n > 0)
 	{
-		while ( n --)
+		while (n--)
 			_putchar('_');
 	}
 	_putchar('\n');
