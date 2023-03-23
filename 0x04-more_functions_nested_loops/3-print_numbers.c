435f26c2cced0dc8980a01af65193948528b8525
nada <nadaait171@gmail.com>
2023-03-23 07:57:08 -0700

    no

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- *print_numbers - prints 0-9
- *
- *Return: void
+ * print_numbers - print 0-9
+ * return: void
  */
 
 void print_numbers(void)
 {
-	char i;
+	char c;
 
-	for (i = '0' ; i <= '9' ; i++)
-		_putchar(i);
-
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
 	_putchar('\n');
 }
