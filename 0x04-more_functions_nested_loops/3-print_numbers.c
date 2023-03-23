d0254605cc6aa4c6a7029bacb7f8342269919c63
nada <nadaait171@gmail.com>
2023-03-23 07:39:06 -0700

    nio

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+
+	_putchar('\n');
+}
