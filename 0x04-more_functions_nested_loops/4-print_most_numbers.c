93c24f30e7fcbebf1542a4af55a7c264913e17b5
nada <nadaait171@gmail.com>
2023-03-23 08:15:31 -0700

    no

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print num except 2 and 4
+ *print_most_numbers - print num except 2 and 4
  *
- * return: void
+ *Return: void
  */
 
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48 ; i <= 57  ; i++)
+	for (i = 48 ; i < 58 ; i++)
 	{
 		if (i != 50 && i != 52)
 			_putchar(i);
