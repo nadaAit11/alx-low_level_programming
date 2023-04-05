929af66501040165b750c0e71a1444429060eb43
nada <nadaait171@gmail.com>
2023-04-05 11:16:44 -0700

    n

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * print_rev_recursion - prints a reverse string
+ * _print_rev_recursion - prints a reverse string
  * @s: variable
  * Return: void
  */
 void _print_rev_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
