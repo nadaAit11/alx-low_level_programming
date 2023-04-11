da2586754e8d67565a09fd6de08278fe5575dd60
nada <nadaait171@gmail.com>
2023-04-10 19:59:37 -0700

    nn

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,14 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
 /**
  * main - Entry point
- *
+ *@str: string
  * Return: Always 0 (Success)
  */
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
 }
