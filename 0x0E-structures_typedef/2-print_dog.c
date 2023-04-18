3651f99d052e375cc5b30d22ded3d04bc7be105e
nada <nadaait171@gmail.com>
2023-04-17 18:46:54 -0700

    n

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -10,17 +11,17 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		printf("nothing");
+		return;
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age == NULL)
-                printf("Age: (nil)");
+                printf("Age: (nil)\n");
         else
                 printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-                printf("Owner: (nil)");
+                printf("Owner: (nil)\n");
         else
                 printf("Owner: %s\n", d->owner);
 }
