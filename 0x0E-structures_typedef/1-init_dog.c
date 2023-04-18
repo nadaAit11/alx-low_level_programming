ab2eddbe23f561a286b265c74308cd6fdb772b4d
nada <nadaait171@gmail.com>
2023-04-17 18:16:57 -0700

    n

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ * init_dog - initialize a structure
+ * @d: a pointer
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
