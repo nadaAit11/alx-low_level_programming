a4f3390e3cfdf180a70d8774978850bdb64f2ba5
nada <nadaait171@gmail.com>
2023-04-20 19:14:26 -0700

    g

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,14 +19,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		sep = "";
 	else
 		sep = (char *) separator;
-	
 	va_start(li, n);
 	for (i = 0 ; i < n ; i++)
 	{
 		if (i == 0)
-			printf("%d", va_arg(li ,int));
+			printf("%d", va_arg(li, int));
 		else
-			printf("%s%d", sep,  va_arg(li, int));
+			printf("%s%d", sep, va_arg(li, int));
 	}
 	va_end(li);
 	printf("\n");
