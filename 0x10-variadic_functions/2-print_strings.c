34509554f39696a699a7394eda0e7e570d5bc42a
nada <nadaait171@gmail.com>
2023-04-20 19:44:48 -0700

    h

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,17 +21,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		sep = (char *) separator;
 
 	va_start(li, n);
-
-	for (i = 0 ; i < n ; i++)
+	if (n > 0)
+		printf("%s", va_arg(list, char *));
+	for (i = 1 ; i < n ; i++)
 	{
 		e = va_arg(li, char *);
 
 		if (e == NULL)
 			e = "nil";
-		if (i == 0)
-			printf("%s", e);
-		else
-			printf("%s%s", sep, e);
+
+		printf("%s%s", sep, e);
 	}
 	va_end(li);
 	printf("\n");
