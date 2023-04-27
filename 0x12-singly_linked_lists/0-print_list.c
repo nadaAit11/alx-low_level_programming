2feca02c1c640e276feec1e2bbec0817ec3c09f1
Nada <nadaait171@gmail.com>
2023-04-27 16:56:27 -0700

    length

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: list of elements
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		s++;
+		h = h->next;
+	}
+	return (s);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,7 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
+#endif
