9e23c77a7bd3af43d2dbe73b1d6ab3ee1d67fdd2
Nada <nadaait171@gmail.com>
2023-04-29 11:29:09 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -17,8 +17,8 @@ size_t print_listint(const listint_t *h)
 	while (h)
 	{
 		printf("%i\n", h->n);
-		h = h->next;
 		len++;
+		h = h->next;
 	}
 	return (len);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
