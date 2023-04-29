23bd94cf328700cf121a327464b13314cd5179a1
Nada <nadaait171@gmail.com>
2023-04-29 12:25:12 -0700

    free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees listint_t list.
+ * @head: pointer to the first element of the list.
+ * Return: void
+ **/
+
+void free_listint(listint_t *head)
+{
+	listint_t *new, *actual;
+
+	if (head)
+	{
+		actual = head;
+		new = head->next;
+		while(new)
+		{
+			free(actual);
+			actual = new;
+			new = new->next;
+		}
+		free(actual);
+	}
+}
