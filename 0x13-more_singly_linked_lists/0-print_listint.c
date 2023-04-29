c60fe159e1751cb4eafdc645eb001a4d5a67b53e
Nada <nadaait171@gmail.com>
2023-04-29 11:16:04 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: a pointer to a list of inetegers
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		printf("%i\n", h->str);
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
