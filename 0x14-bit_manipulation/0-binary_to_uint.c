f8b6bb4c25d63311a136fb8ceb91242d123d2f58
Nada <nadaait171@gmail.com>
2023-05-13 12:40:06 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,10 +13,10 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int dec = 0;
 
-	if (!b || b == NULL)
+	if (!b)
 		return (0);
 
-	for (i = 0; i < b[i] ; i++)
+	for (i = 0; b[i] ; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
